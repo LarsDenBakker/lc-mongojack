6222b6acc5072168675986ee102d1798eb8efc80
Lars den Bakker <larsdenbakker@gmail.com>
2016-05-30 11:32:17 +0200

    Switch to key-value naming conventions.

--- src/main/java/com/larscode/mongo/MongoRepository.java
+++ src/main/java/com/larscode/mongo/MongoRepository.java
@@ -3,66 +3,65 @@ package com.larscode.mongo;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.MongoException;
-import org.bson.types.ObjectId;
 import org.mongojack.JacksonDBCollection;
 
 import java.lang.reflect.InvocationTargetException;
 
-public class MongoRepository<T extends MongoObject<K>, K> {
+public class MongoRepository<K, V extends MongoObject<K>> {
 
     private final DB mongoDB;
 
-    private final Class<T> type;
+    private final Class<K> keyType;
+    private final Class<V> valueType;
     private final String collectionName;
-    private final JacksonDBCollection<T, K> collection;
-    private final Class<K> idType;
+    private final JacksonDBCollection<V, K> collection;
 
-    public MongoRepository(DB mongoDB, Class<T> type, Class<K> idType, String collectionName) {
-        this.type = type;
-        this.idType = idType;
+    public MongoRepository(DB mongoDB, Class<K> keyType, Class<V> valueType, String collectionName) {
+        this.keyType = keyType;
+        this.valueType = valueType;
         this.collectionName = collectionName;
         this.mongoDB = mongoDB;
         this.collection =  configureCollection();
     }
 
-    public MongoRepository(DB mongoDB, Class<T> type, Class<K> idType) {
-        this(mongoDB, type, idType, type.getSimpleName().toLowerCase());
+    public MongoRepository(DB mongoDB, Class<K> keyType, Class<V> valueType) {
+        this(mongoDB, keyType, valueType, valueType.getSimpleName().toLowerCase());
     }
 
-    public Class<T> getType() {
-        return type;
+    public Class<V> getValueType() {
+        return valueType;
     }
 
     public String getCollectionName() {
         return collectionName;
     }
 
-    public JacksonDBCollection<T, K> getCollection() {
+    public JacksonDBCollection<V, K> getCollection() {
         return collection;
     }
 
-    public T createNew() throws IllegalArgumentException {
+    public V createNew() throws IllegalArgumentException {
         try {
-            T t = instantiate();
-            return collection.insert(t).getSavedObject();
+            V v = instantiate();
+            return collection.insert(v).getSavedObject();
         } catch (MongoException e) {
             throw creationException(e);
         }
     }
 
-    private T instantiate() throws IllegalArgumentException {
+    private V instantiate() throws IllegalArgumentException {
         try {
-            return type.getConstructor().newInstance();
+            return valueType.getConstructor().newInstance();
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
             throw creationException(e);
         }
     }
 
     private IllegalArgumentException creationException(Exception cause) throws IllegalArgumentException {
-        return new IllegalArgumentException("Cannot create instance of MongoObject type: " + type, cause);
+        return new IllegalArgumentException("Cannot create instance of MongoObject valueType: " + valueType, cause);
     }
 
-    private JacksonDBCollection<T, K> configureCollection() {
+    private JacksonDBCollection<V, K> configureCollection() {
         return getJacksonDBCollection(getDBCollection());
     }
 
@@ -74,8 +73,8 @@ public class MongoRepository<T extends MongoObject<K>, K> {
         }
     }
 
-    private JacksonDBCollection<T, K> getJacksonDBCollection(final DBCollection dbCollection) {
-        return JacksonDBCollection.wrap(dbCollection, type, idType);
+    private JacksonDBCollection<V, K> getJacksonDBCollection(final DBCollection dbCollection) {
+        return JacksonDBCollection.wrap(dbCollection, valueType, keyType);
     }
 
 
